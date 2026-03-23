5b659a354aa966aa74b8575059234fe28206ba26
unknown <matheusmag.dev@gmail.com>
2026-03-23 13:05:59 -0300

    Adição, exclusão e edição de tarefas

--- src/app/pages/consultar-tarefa/consultar-tarefa.ts
+++ src/app/pages/consultar-tarefa/consultar-tarefa.ts
@@ -3,13 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { Navbar } from '../../layout/navbar/navbar';
 import { Sidebar } from '../../layout/sidebar/sidebar';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consultar-tarefa',
   imports: [
     Navbar,
     Sidebar,
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './consultar-tarefa.html',
   styleUrl: './consultar-tarefa.css',
@@ -22,12 +25,67 @@ export class ConsultarTarefa {
   //HTTP CLIENT para integração com a API:
   private http = inject(HttpClient);
 
+  //Formulário para filtrar as datas da pesquisa
+  formulario = new FormGroup({
+    dataMin: new FormControl('', [Validators.required]),
+    dataMax: new FormControl('', [Validators.required]),
+  });
+
   //Função executada ao abrir a página
   ngOnInit() {
-    //Executar a consulta na API dae tarefas
-    this.http.get('http://localhost:8083/api/v1/tarefas')
+
+    //Pegar a data atual
+    const hoje = new Date();
+
+    //Primeiro dia do mês
+    const primeiroDia = new Date(hoje.getFullYear(), hoje.getMonth(), 1);
+
+    //Último dia do mês
+    const ultimoDia = new Date(hoje.getFullYear(), hoje.getMonth() + 1, 0);
+
+    //Formatando as datas para yyyy-MM-dd
+    const formatarData = (data: Date) => {
+      return data.toISOString().split('T')[0];
+    }
+
+    //Preencher o fomulário com as datas do primeiro e último dia do mês
+    this.formulario.patchValue({
+      dataMin: formatarData(primeiroDia),
+      dataMax: formatarData(ultimoDia)
+    });
+
+    //Realizar a consulta das tarefas do mês atual
+    this.consultar();
+
+  }
+
+  //Função para executar a consulta das tarefas
+  consultar() {
+    //Capturar as datas do formulário
+    const dataMin = this.formulario.value.dataMin;
+    const dataMax = this.formulario.value.dataMax;
+
+    //Executar a consulta na API de tarefas
+    this.http.get('http://localhost:8083/api/v1/tarefas/' + dataMin + "/" + dataMax)
       .subscribe((data) => {
         this.tarefas.set(data as any[]);
       });
+  }
+
+
+  //Função para excluir a tarefa
+  excluir(id: string) {
+    if (confirm('Deseja realmente excluir a tarefa selecionada?')) {
+      this.http.delete('http://localhost:8083/api/v1/tarefas/' + id)
+        .subscribe({
+          next: (data: any) => {
+            alert('Tarefa: ' + data.nome + ", excluída com sucesso!");
+            this.consultar();
+          },
+          error: (e) => {
+            console.log(e.error);
+          }
+        });
+    }
   }
 }
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,15 +2,35 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { Navbar } from '../../layout/navbar/navbar';
 import { Sidebar } from '../../layout/sidebar/sidebar';
+import { Chart, ChartModule } from 'angular-highcharts';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     CommonModule,
     Navbar,
-    Sidebar
+    Sidebar,
+    FormsModule,
+    ReactiveFormsModule,
+    ChartModule
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard {
+
+  graficoDonut = signal<Chart>(new Chart());
+  graficoColunas = signal<Chart>(new Chart());
+
+  formulario = new FormGroup({
+
+    dataMin : new FormControl('', [Validators.required]),
+    dataMax : new FormControl('', [Validators.required]),
+
+  });
+
+  consultar() {
+    
+  };
+}
